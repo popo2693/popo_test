0aa5934ef894834a2a3e811f691adcef1f46e0a2
popo2693 <popo2693@naver.com>
2021-09-03 17:41:42 +0900

    [정산][1.1.0] git기능구현

--- sex.py
+++ sex.py
@@ -1,1 +1,18 @@
+import os, pandas
+
+print('hi guzzang')
+
 print('hi popo')
+
+print('bug fixed')
+# 운영중:메인
+# 다음버전배포대기 : 베타
+# 기능단위 : CODE_N
+# 최신배포: RELEASE_1.10
+# 버그픽스: 최신배포 브랜치(RELEASE1.1)에서 추가 커밋
+# 메인 <- RELEASE_1.10
+# 1. 베타 <- 메인 MERGE
+# 2. 베타 REBASE 메인
+
+## 가이드2
+# FEATURE 1.0 에서 기능 추가구현 확인 (브렌치 추가)
