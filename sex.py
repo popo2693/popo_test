a59f05ee6d90c87349dda9fc2c372d2f26f13517
popo2693 <popo2693@naver.com>
2021-09-04 14:10:57 +0900

    (가이드) 브랜치 사용 전략

--- sex.py
+++ sex.py
@@ -4,3 +4,12 @@ print('hi guzzang')
 
 print('hi popo')
 
+# 운영중:메인
+# 다음버전배포대기 : 베타
+# 기능단위 : CODE_N
+# 최신배포: RELEASE_1.10
+# 버그픽스: 최신배포 브랜치(RELEASE1.1)에서 추가 커밋
+# 메인 <- RELEASE_1.10
+# 1. 베타 <- 메인 MERGE
+# 2. 베타 REBASE 메인
+
